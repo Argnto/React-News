e2a3ee5d05fa9f48745f69f4f8ca937acb4c170b
Martin <argentoar@hotmail.com.ar>
2021-02-13 02:29:04 -0300

    agregado layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>React News</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           React News
         </h1>
       </main>
