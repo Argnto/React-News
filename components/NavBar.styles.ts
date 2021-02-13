c50c32c41442b65492ea419a436b4d1ba9592849
Javier Diaz <diazjavierdavid@hotmail.com>
2021-02-13 16:24:53 -0300

    Se agrega GlobalStyle

---
+++ components/NavBar.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+    width: 100%;
+    height: 60px;
+    border: 2px solid red;
+`
+export const Logo = styled.div`
+    
+`
