1899aff236d2a79a4cdd54607be5d498b1c2e971
Javier Diaz <diazjavierdavid@hotmail.com>
2021-02-13 22:56:58 -0300

    Navbar styled

--- components/NavBar.styles.ts
+++ components/NavBar.styles.ts
@@ -1,7 +1,80 @@
 import styled from 'styled-components';
 
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 10%;
+    background-color: #24252A;
+
+    li , a , button {
+        font-weight: 500;
+        font-size: 18px;
+        color: white;
+        text-decoration: none;
+    }
+`;
+
 export const Nav = styled.nav`
-    width: 100%;
-    border: 2px solid red;
-    height: 60px;
+
+`;
+
+export const Logo = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    h2 {
+        font-size: 30px;
+        color: white;
+        padding-left: 10px;
+    }
+`;
+
+export const NavList = styled.ul`
+    list-style:none;
+`;
+
+export const NavItem = styled.li`
+    display: inline-block;
+    padding: 0px 20px;
+
+    a {
+        transition: all 0.3s ease 0s;
+    }
+
+    a:hover{
+        color: #0088A9;
+    }
+`;
+
+export const SearchBox = styled.div`
+    background: #2f3640;
+    border-radius: 40px;
+    padding: 5px;
+    display: flex;
 `
+
+export const SearchBar = styled.input`
+    border: none;
+    background: transparent;
+    outline: none;
+    padding: 0;
+    color: white;
+    font-size: 16px;
+    margin: auto 10px;
+`
+
+export const SearchBtn = styled.a`
+    padding: 10px 12px;
+    background-color: rgba(0,126,169,1);
+    border: none;
+    border-radius: inherit;
+    cursor: pointer;
+    transition: all 0.3s ease 0s;
+    outline: none;
+    
+    &:hover {
+        background-color: rgba(0,126,169,0.8);
+    }
+`;
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,37 @@
-import { Nav } from './NavBar.styles';
+import { Header, Logo, Nav, NavList, NavItem, SearchBox, SearchBtn, SearchBar } from './NavBar.styles';
+import Link from 'next/link';
+import Image from 'next/image';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const NavBar: React.FC = () => {
     return (
+      <Header>
+          <Logo>
+              <Image 
+                src="/logo1.png"
+                width={60}
+                height={50} />
+              <h2>React News</h2>
+          </Logo>
         <Nav>
-        <div>NavBar</div>
+            <NavList>
+                <NavItem>
+                    <Link href="/"><a>Noticias del día</a></Link>
+                </NavItem>
+                <NavItem>
+                    <Link href="/memes"><a>Memes</a></Link>
+                </NavItem>
+                <NavItem>
+                    <Link href="/about"><a>About</a></Link>
+                </NavItem>
+            </NavList>
         </Nav>
+            <SearchBox>
+                <SearchBar type="text" placeholder="Busca tu noticia" />
+                <SearchBtn> <FontAwesomeIcon icon={faSearch} /> </SearchBtn>
+            </SearchBox>
+      </Header>
     );
 }
  
